19b240499fed24a559f3e9adff2dd763c01673cc
System Administrator <root@soutekiMacBook-Pro.local>
2017-02-21 13:58:00 -0800

    02/21/2017 v1.4

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -120,3 +120,108 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	return valAsbytes, nil
 }
 
+
+// ============================================================================================================================
+// Init Amount - set the initial amount for user
+// ============================================================================================================================
+func (t *SimpleChaincode) init_amount(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+
+	//   0      1 
+	// "bob", "200.45"
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+
+	//input sanitation
+	fmt.Println("- start init amount")
+	if len(args[0]) <= 0 {
+		return nil, errors.New("1st argument must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return nil, errors.New("2nd argument must be a non-empty string")
+	}
+	
+	name := args[0]
+	amount,err := strconv.ParseFloat(args[1], 64)
+	if err != nil {
+		return nil, errors.New("2nd argument must be a numeric string")
+	}
+	amountStr := strconv.FormatFloat(amount, 'E', -1, 64)
+	err = stub.PutState(name, []byte(amountStr))									//store marble with id as key
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("- end init amount")
+	return nil, nil
+}
+
+// ============================================================================================================================
+// Transfer money from user A to user B
+// ============================================================================================================================
+func (t *SimpleChaincode) transfer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+	var userA, userB, jsonResp string
+	var newAmountA, newAmountB float64
+	
+	//    0       1      2
+	// "alice", "bob", "12.56"
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+	
+	fmt.Println("- start transfer money")
+	fmt.Println("- from " + args[0] + " to " + args[1])
+
+	userA = args[0]
+	userB = args[1]
+	amount,err := strconv.ParseFloat(args[2], 64)
+	if err != nil {
+		return nil, errors.New("3rd argument must be a numeric string")
+	}
+	amountByteA, err := stub.GetState(userA)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + userA + "\"}"
+		return nil, errors.New(jsonResp)
+	}	
+	amountByteB, err := stub.GetState(userB)	
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + userB + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	amountStrA := string(amountByteA[:])
+	amountStrB := string(amountByteB[:])
+	amountA,err := strconv.ParseFloat(amountStrA, 64)
+	if err != nil {
+		return nil, err
+	}
+	amountB,err := strconv.ParseFloat(amountStrB, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	if (amountA - amount) < 0 {
+		return nil, errors.New(args[0] + " doesn't have enough balance to complete transaction")
+	} 
+	newAmountA = amountA - amount
+	newAmountB =  amountB + amount
+	newAmountStrA := strconv.FormatFloat(newAmountA, 'E', -1, 64)
+	newAmountStrB := strconv.FormatFloat(newAmountB, 'E', -1, 64)
+
+
+	err = stub.PutState(args[0], []byte(newAmountStrA))		
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stub.PutState(args[1], []byte(newAmountStrB))		
+
+	if err != nil {
+		return nil, err
+	}
+	
+	fmt.Println("- transfer completed")
+	return nil, nil
+}
