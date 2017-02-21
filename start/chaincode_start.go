08a3e1c08d96894145eb9f07847abe2c3cb2e09f
System Administrator <root@soutekiMacBook-Pro.local>
2017-02-21 13:52:34 -0800

    02/21/2017 v1.2

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -40,84 +40,86 @@ func main() {
 	}
 }
 
-// ============================================================================================================================
-// Init - reset all the things
-// ============================================================================================================================
+// Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	var err error
-
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	// Write the state to the ledger
-	err = stub.PutState("testKey", []byte(args[0]))				//making a test var "testKey", I find it handy to read/write to it right away to test the network
+	err := stub.PutState("test_key", []byte(args[0]))
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return nil, nil
 }
 
-// ============================================================================================================================
-// Invoke - Our entry point for Invocations
-// ============================================================================================================================
+// Invoke isur entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
 	// Handle different functions
-	if function == "init" {													
+	if function == "init" {
 		return t.Init(stub, "init", args)
-	} else if function == "delete" {										
-		return t.delete(stub, args)
-	} else if function == "write" {								
+	} else if function == "write" {
 		return t.write(stub, args)
-	} else if function == "read" {								
-		return t.read(stub, args)
-	} else if function == "init_amount" {									
-		return t.init_amount(stub, args)
-	} else if function == "transfer" {										
-		return t.transfer(stub, args)
+	} else if function == "delete" {
+		return t.write(stub, args)
 	}
-	fmt.Println("invoke did not find func: " + function)					//error
+	fmt.Println("invoke did not find func: " + function)
 
-	return nil, errors.New("Received unknown function invocation")
+	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
-// ============================================================================================================================
-// Query - Our entry point for Queries
-// ============================================================================================================================
+// Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "read" {													//read a variable
+	if function == "read" { //read a variable
 		return t.read(stub, args)
 	}
-	fmt.Println("query did not find func: " + function)						//error
+	fmt.Println("query did not find func: " + function)
 
-	return nil, errors.New("Received unknown function query")
+	return nil, errors.New("Received unknown function query: " + function)
 }
 
-// ============================================================================================================================
-// Read - read a variable from chaincode state
-// ============================================================================================================================
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var name, jsonResp string
+	var key, jsonResp string
 	var err error
 
 	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
-	name = args[0]
-	valAsbytes, err := stub.GetState(name)									//get the var from chaincode state
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	return valAsbytes, nil													//send it onward
+	return valAsbytes, nil
 }
 
 // ============================================================================================================================
@@ -137,128 +139,3 @@ func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string
 	return nil, nil
 }
 
-// ============================================================================================================================
-// Write - write variable into chaincode state
-// ============================================================================================================================
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var name, value string // Entities
-	var err error
-	fmt.Println("running write()")
-
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
-	}
-
-	name = args[0]															//rename for funsies
-	value = args[1]
-	err = stub.PutState(name, []byte(value))								//write the variable into the chaincode state
-	if err != nil {
-		return nil, err
-	}
-	return nil, nil
-}
-
-// ============================================================================================================================
-// Init Amount - set the initial amount for user
-// ============================================================================================================================
-func (t *SimpleChaincode) init_amount(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var err error
-
-	//   0      1 
-	// "bob", "200.45"
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2")
-	}
-
-	//input sanitation
-	fmt.Println("- start init amount")
-	if len(args[0]) <= 0 {
-		return nil, errors.New("1st argument must be a non-empty string")
-	}
-	if len(args[1]) <= 0 {
-		return nil, errors.New("2nd argument must be a non-empty string")
-	}
-	
-	name := args[0]
-	amount,err := strconv.ParseFloat(args[1], 64)
-	if err != nil {
-		return nil, errors.New("2nd argument must be a numeric string")
-	}
-	amountStr := strconv.FormatFloat(amount, 'E', -1, 64)
-	err = stub.PutState(name, []byte(amountStr))									//store marble with id as key
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println("- end init amount")
-	return nil, nil
-}
-
-// ============================================================================================================================
-// Transfer money from user A to user B
-// ============================================================================================================================
-func (t *SimpleChaincode) transfer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var err error
-	var userA, userB, jsonResp string
-	var newAmountA, newAmountB float64
-	
-	//    0       1      2
-	// "alice", "bob", "12.56"
-	if len(args) != 3 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 3")
-	}
-	
-	fmt.Println("- start transfer money")
-	fmt.Println("- from " + args[0] + " to " + args[1])
-
-	userA = args[0]
-	userB = args[1]
-	amount,err := strconv.ParseFloat(args[2], 64)
-	if err != nil {
-		return nil, errors.New("3rd argument must be a numeric string")
-	}
-	amountByteA, err := stub.GetState(userA)
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + userA + "\"}"
-		return nil, errors.New(jsonResp)
-	}	
-	amountByteB, err := stub.GetState(userB)	
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + userB + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-	amountStrA := string(amountByteA[:])
-	amountStrB := string(amountByteB[:])
-	amountA,err := strconv.ParseFloat(amountStrA, 64)
-	if err != nil {
-		return nil, err
-	}
-	amountB,err := strconv.ParseFloat(amountStrB, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	if (amountA - amount) < 0 {
-		return nil, errors.New(args[0] + " doesn't have enough balance to complete transaction")
-	} 
-	newAmountA = amountA - amount
-	newAmountB =  amountB + amount
-	newAmountStrA := strconv.FormatFloat(newAmountA, 'E', -1, 64)
-	newAmountStrB := strconv.FormatFloat(newAmountB, 'E', -1, 64)
-
-
-	err = stub.PutState(args[0], []byte(newAmountStrA))		
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = stub.PutState(args[1], []byte(newAmountStrB))		
-
-	if err != nil {
-		return nil, err
-	}
-	
-	fmt.Println("- transfer completed")
-	return nil, nil
-}
